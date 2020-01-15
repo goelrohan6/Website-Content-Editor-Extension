dad1ee1b1497b3ae23407475cda775ddcafecfb5
goelrohan6 <goelrohan6@gmail.com>
2020-01-16 04:55:35 +0530

    feat: added notifications

--- tab_shortcuts.js
+++ tab_shortcuts.js
@@ -1,19 +1,25 @@
 const tabDesignModeMapping = {};
 let activeTabId;
 
-function setDesignModeOff(tabId) {
+function setDesignModeOff(tabId, showNotification = false) {
   tabDesignModeMapping[tabId] = "off";
   chrome.tabs.executeScript({
     code: 'document.designMode="off"'
   });
   chrome.browserAction.setIcon({path: "icon-off.png"});
+  if (showNotification) {
+    createNotification("off");
+  }
 }
-function setDesignModeOn(tabId) {
+function setDesignModeOn(tabId, showNotification = false) {
   tabDesignModeMapping[tabId] = "on";
   chrome.tabs.executeScript({
     code: 'document.designMode="on"'
   });
   chrome.browserAction.setIcon({path: "icon-on.png"});
+  if (showNotification) {
+    createNotification("on");
+  }
 }
 
 function getDesignModeForTab(tabId) {
@@ -37,6 +43,26 @@ function setActiveTab(tabId) {
     }
   }
 }
+function createNotification(type) {
+  let title;
+  let message;
+  const notificationType = "basic";
+  if (type === "on") {
+    title = "Content edit on ⚡️";
+    message = "Just click on the content to edit it :)";
+    iconUrl = "icon-on.png";
+  } else {
+    title = "Content edit off ❌";
+    message = "Press CMD + SHIFT + X to turn on content edit again."
+    iconUrl = "icon-off.png";
+  }
+  chrome.notifications.create({
+    type: notificationType,
+    iconUrl,
+    title,
+    message
+  })
+}
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.status == 'complete') {
     setActiveTab(tabId);
@@ -50,9 +76,9 @@ chrome.commands.onCommand.addListener(function(command) {
   console.log("chrome.tabs", chrome.tabs)
   if (command === "toggle-designMode") {
     if (getDesignModeForTab(activeTabId) === "on") {
-      setDesignModeOff(activeTabId);
+      setDesignModeOff(activeTabId, true);
     } else {
-      setDesignModeOn(activeTabId);
+      setDesignModeOn(activeTabId, true);
     }
   }
 });
